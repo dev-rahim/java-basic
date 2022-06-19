5e7706beb8f7ba6d9f1888da49148011b7d81d1c
dev-rahim <rahimuddin5915@gmail.com>
2022-06-20 00:44:16 +0600

    exploring data types

---
+++ dataTypes.java
@@ -0,0 +1,11 @@
+public class dataTypes {
+    public static void main(String[] args) {
+        String Name = "Rahim";
+        int roll =125;
+        float weeight = 60.5f;
+        double price = 500.00;
+        Character group='b';
+        Long numLong=123456789523654l;
+        boolean isTrue=false; 
+    }
+}
