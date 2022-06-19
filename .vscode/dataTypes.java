cf7f703981e52c0404e3b8eb3af7e1ce4c4b8e35
dev-rahim <rahimuddin5915@gmail.com>
2022-06-20 00:36:47 +0600

    first commit

---
+++ .vscode/dataTypes.java
@@ -0,0 +1,21 @@
+/**
+ * dataTypes
+ */
+public class dataTypes {
+
+    public static void main(String[] args) {
+        String name = "Rahim Uddin";
+        int roll=127219;
+        float weight=65.05f;
+        Double price=500d;
+        char group='B';
+        Boolean RahimIs = true;
+        System.out.println(name);
+        System.out.println(roll);
+        System.out.println(weight);
+        System.out.println(price);
+        System.out.println(group);
+        System.out.println(RahimIs);
+
+    }
+}
---
+++ variable.java
@@ -0,0 +1,11 @@
+/**
+ * variable
+ */
+public class variable {
+    static String Name = "Rahim"; //static variable
+    String details ="Hi My name is Rahim"; //instance variable
+    public static void main(String[] args) {
+        Double Price = 500; //local variable
+        
+    }
+}
